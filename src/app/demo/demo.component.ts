110f33c4a3f9dae37211a1f3f46860d84818ad48
ANANTH4004 <balamuruganananth@gmail.com>
2024-01-22 21:09:20 +0530

    sweet alert - npm install -g sweetalert2

--- src/app/demo/demo.component.ts
+++ src/app/demo/demo.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-demo',
@@ -6,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./demo.component.scss']
 })
 export class DemoComponent {
+
 questions = [
     {
       description: 'What is your favorite color?',
@@ -17,4 +19,42 @@ questions = [
     },
     // Add more questions as needed
   ];
+
+
+  simpleAlert(){
+    Swal.fire('Hello world!');
+  }
+
+  alertWithSuccess(){
+    Swal.fire({
+      title: "Good job!",
+      text: "You clicked the button!",
+      icon: "success"
+    });
+  }
+
+  // confirmBox(){
+  //   Swal.fire({
+  //     title: 'Are you sure want to remove?',
+  //     text: 'You will not be able to recover this file!',
+  //     icon: 'warning',
+  //     showCancelButton: true,
+  //     confirmButtonText: 'Yes, delete it!',
+  //     cancelButtonText: 'No, keep it'
+  //   }).then((result) => {
+  //     if (result.value) {
+  //       Swal.fire(
+  //         'Deleted!',
+  //         'Your imaginary file has been deleted.',
+  //         'success'
+  //       )
+  //     } else if (result.dismiss === Swal.DismissReason.cancel) {
+  //       Swal.fire(
+  //         'Cancelled',
+  //         'Your imaginary file is safe :)',
+  //         'error'
+  //       )
+  //     }
+  //   })
+  // }
 }
